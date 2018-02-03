df1cee53d83a77d57037c1140d8d0c74c34070d0
daviddalbusco <david.dalbusco@outlook.com>
2018-02-03 13:12:03 +0100

    No test implementation yet

--- src/components/web-photo-filter/web-photo-filter.spec.ts
+++ src/components/web-photo-filter/web-photo-filter.spec.ts
@@ -15,27 +15,6 @@ describe('web-photo-filter', () => {
       });
     });
 
-    it('should work without parameters', () => {
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m');
-    });
-
-    it('should work with a first name', async () => {
-      element.first = 'Peter';
-      await flush(element);
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter');
-    });
-
-    it('should work with a last name', async () => {
-      element.last = 'Parker';
-      await flush(element);
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m  Parker');
-    });
-
-    it('should work with both a first and a last name', async () => {
-      element.first = 'Peter'
-      element.last = 'Parker';
-      await flush(element);
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter Parker');
-    });
+    // Not implement yet
   });
 });
